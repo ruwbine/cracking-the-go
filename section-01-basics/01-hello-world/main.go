c79a4717ade3c733f8a55258396116f8ce1b44be
aetery- <kurthamachi@mail.ru>
2025-06-06 21:11:38 +0500

    Added new solution: main.go

--- section-01-basics/01-hello-world/main.go
+++ section-01-basics/01-hello-world/main.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-func swap(x, y string) (string, string){
-	return y, x;
+func main() {
+	fmt.Println(ZipString("Heellooo World!"))
+	fmt.Println(ZipString("aaaaa"))
+	fmt.Println(ZipString("Hellloo FElllllasss"))
+	fmt.Println(ZipString("aaaaa"))
 }
 
-func main(){
-	a, b := swap("Hello", "world")
-	fmt.Println(a,b)
+func ZipString(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+
+	result := ""
+	var count int = 1
+
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			count++
+
+		} else {
+			result += strconv.Itoa(count)
+			result += string(s[i-1])
+			count = 1
+		}
+	}
+	result += strconv.Itoa(count)
+	result += string(s[len(s)-1])
+	return result
 }
